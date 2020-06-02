0569a765c9b143828a4835267ea4c6b73faf2a91
Jordan M <jordan.muliaman@gmail.com>
2020-06-02 09:36:41 -0700

    username working

--- public/app.js
+++ public/app.js
@@ -1,69 +1,23 @@
-const checkUser = () => {
-    if (localStorage.getItem('user')) {
-      axios.get(`/api/users/${localStorage.getItem('user')}`)
-        .then(({ data }) => {
-          document.getElementById('user')
-          
-          localStorage.setItem('user', data.user.id)
-
-          // data.groceries.forEach(grocery => {
-          //   let groceryElem = document.createElement('li')
-          //   groceryElem.textContent = `
-          //   ${grocery.name} | Quantity: ${grocery.quantity}
-          // `
-          //   document.getElementById('groceries').append(groceryElem)
-          // })
-
-          document.getElementById('signUpForm').style.display = 'none'
-          document.getElementById('signInForm').style.display = 'none'
-          document.getElementById('signOut').style.display = 'block'
-        })
-        .catch(err => console.error(err))
-    }
-  }
-
-  document.getElementById('signIn').addEventListener('click', event => {
+  document.getElementById('signUp').addEventListener('click', event => {
     event.preventDefault()
-    axios.get(`/api/users/${document.getElementById('username').value}`)
+    axios.post('/api/users', {
+      name: document.getElementById('newUsername').value
+    })
       .then(({ data }) => {
-
-        localStorage.setItem('user', data.user.id)
-
-        // data.groceries.forEach(grocery => {
-        //   let groceryElem = document.createElement('li')
-        //   groceryElem.textContent = `
-        //     ${grocery.name} | Quantity: ${grocery.quantity}
-        //   `
-        //   document.getElementById('groceries').append(groceryElem)
-        // })
-
-        document.getElementById('signUpForm').style.display = 'none'
-        document.getElementById('signInForm').style.display = 'none'
-        document.getElementById('signOut').style.display = 'block'
+        // console.log(data)
+        localStorage.setItem('users', data.id)
+        window.location = '/deals.html'
       })
       .catch(err => console.error(err))
   })
-  
-  
-    document.getElementById('signUp').addEventListener('click', event => {
-      event.preventDefault()
-      axios.post('/api/users', {
-        username: document.getElementById('newUsername').value
+
+  document.getElementById('signIn').addEventListener('click', event => {
+    event.preventDefault()
+    axios.get(`api/login/${document.getElementById('username').value}`)
+      .then(({ data }) => {
+        // console.log(data)
+        localStorage.setItem('users', data.id)
+        window.location = '/deals.html'
       })
-        .then(({ data }) => {
-          localStorage.setItem('user', data.insertId)
-          document.getElementById('signUpForm').style.display = 'none'
-          document.getElementById('signInForm').style.display = 'none'
-          document.getElementById('signOut').style.display = 'block'
-        })
-    })
-      
-  document.getElementById('signOut').addEventListener('click', () => {
-    localStorage.removeItem('user')
-    // document.getElementById('groceries').innerHTML = ''
-    document.getElementById('signUpForm').style.display = 'block'
-    document.getElementById('signInForm').style.display = 'block'
-    document.getElementById('signOut').style.display = 'none'
+      .catch(err => console.error(err))
   })
-      
-  checkUser()
