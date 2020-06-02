a87f0b5152f65f5383af5e9599d3168e46a276de
vertjames <ianandrewkirby@gmail.com>
2020-06-02 14:33:34 -0700

    started deals.html w/ add'l css and js files

--- public/app.js
+++ public/app.js
@@ -1,23 +1,23 @@
-  document.getElementById('signUp').addEventListener('click', event => {
-    event.preventDefault()
-    axios.post('/api/users', {
-      name: document.getElementById('newUsername').value
+document.getElementById('signUp').addEventListener('click', event => {
+  event.preventDefault()
+  axios.post('/api/users', {
+    name: document.getElementById('newUsername').value
+  })
+    .then(({ data }) => {
+      // console.log(data)
+      localStorage.setItem('users', data.id)
+      window.location = '/deals.html'
     })
-      .then(({ data }) => {
-        // console.log(data)
-        localStorage.setItem('users', data.id)
-        window.location = '/deals.html'
-      })
-      .catch(err => console.error(err))
-  })
+    .catch(err => console.error(err))
+})
 
-  document.getElementById('signIn').addEventListener('click', event => {
-    event.preventDefault()
-    axios.get(`api/login/${document.getElementById('username').value}`)
-      .then(({ data }) => {
-        // console.log(data)
-        localStorage.setItem('users', data.id)
-        window.location = '/deals.html'
-      })
-      .catch(err => console.error(err))
-  })
+document.getElementById('signIn').addEventListener('click', event => {
+  event.preventDefault()
+  axios.get(`api/login/${document.getElementById('username').value}`)
+    .then(({ data }) => {
+      // console.log(data)
+      localStorage.setItem('users', data.id)
+      window.location = '/deals.html'
+    })
+    .catch(err => console.error(err))
+})
