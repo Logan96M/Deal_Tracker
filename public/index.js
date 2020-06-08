17a7adc6af41d047880832af64ddfef603129009
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-08 13:23:08 -0700

    fixed errors with adding another user of same name

--- public/index.js
+++ public/index.js
@@ -1,42 +1,40 @@
 // // button functionality for signing up and storing login in Local Storage
 document.getElementById('signUp').addEventListener('click', event => {
-  
   event.preventDefault()
   isNameUnique()
-
 })
-  
-
-
 function isNameUnique(username) {
-
-  console.log(username)
+  // console.log(username)
+  //get all users
   axios.get("/api/users/") 
-
   .then(({data}) => {
-
     console.log(data)
+    //go through array of users and check if matches any existing users
   for (i = 0; i < data.length; i++) {
+    //if matches
     if(data[i].name === document.getElementById("newUsername").value) {
+      //return error, and send back to homepage
       alert("Sorry username already exists... try again");
       console.log("false")
       location.reload()
-      
     }
-  }}) 
+  }
+  //if no match
     console.log("true") 
-     axios.post('/api/users', {
+    //create new username
+    axios.post('/api/users', {
     name: document.getElementById('newUsername').value
     })
-    .then(({ data }) => {
+      .then(({ data }) => {
       // console.log(data)
+      //add key value pair to local storage
       localStorage.setItem('users', data.id)
-     window.location = '/deals.html'
+      //change window to deals
+      window.location = '/deals.html'
   })
-
+}) 
   .catch(err => console.error(err))
 }
-
 // button functionality for signing in and storing login in Local Storage
 document.getElementById('signIn').addEventListener('click', event => {
   event.preventDefault()
