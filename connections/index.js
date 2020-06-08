3aecaf9b82eee2d412a52e17d16cdb27932f4619
Logan Morris <loganmorris96@gmail.com>
2020-06-08 15:04:39 -0700

    added to heroku

--- connections/index.js
+++ connections/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('process.env.JAWSDB_URL || process.env.LOCAL_URL')
+const sequelize = new Sequelize('process.env.JAWSDB_URL')
 
 module.exports = sequelize
