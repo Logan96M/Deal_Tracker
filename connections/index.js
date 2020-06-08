c016f91bd0487a70b25d39834bae176cacfac675
Logan Morris <loganmorris96@gmail.com>
2020-06-08 15:57:21 -0700

    Heroku

--- connections/index.js
+++ connections/index.js
@@ -1,5 +1,9 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('process.env.JAWSDB_URL')
+// new Sequelize("process.env.JAWSDB_URL", null, null, {})
+
+// const sequelize = new Sequelize("process.env.JAWSDB_URL" || "process.env.LOCAL_URL")
+
+const sequelize = new Sequelize( process.env.JAWSDB_URL || process.env.LOCAL_URL )
 
 module.exports = sequelize
